37648d031316a8aa3a86f6fd0eafddb4e4a9706b
kerengaiger <keren.gaiger@gmail.com>
2021-08-19 14:32:00 +0300

    add split char as argument

--- main.py
+++ main.py
@@ -158,6 +158,8 @@ best_parameters, values, _experiment, _cur_model = optimize(
         {"name": "l2_emb", "type": "fixed", "value_type": "float", "value": args.l2_emb},
         {"name": "device", "type": "fixed", "value_type": "str", "value": args.device},
         {"name": "is_final_train", "type": "fixed", "value_type": "bool", "value": False},
+        {"name": "split", "type": "fixed", "value_type": "str", "value": args.split_char},
+
 
     ],
     evaluation_function=train_with_cnfg,
